a78cac37a64b45ec36a57ad6acc060af569387b7
Kayo Gamas <kayogamas@hotmail.com>
2018-04-05 12:25:40 -0300

    Adicionado a solução StackLibrary

---
+++ StackLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackLibrary
+{
+    public class Class1
+    {
+    }
+}
